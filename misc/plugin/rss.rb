9f7e4eef534ab2cafcbbbf491ba916252ae03a69
KITAITI Makoto <KitaitiMakoto@gmail.com>
2011-10-19 15:36:33 +0900

    Remove output for debug

--- misc/plugin/rss.rb
+++ misc/plugin/rss.rb
@@ -94,10 +94,6 @@ def rss
     if_modified_since = nil
   end
 
-  p if_modified_since
-  p last_modified
-  p last_modified <=> if_modified_since
-
   if if_modified_since and last_modified <= if_modified_since
     header['status'] = 'NOT_MODIFIED'
     return ::Hiki::Response.new('', 304, header)
